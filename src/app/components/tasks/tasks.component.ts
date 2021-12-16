7c2e6ac56347934e7a8740c463cd7336e3fb9bbe
yumindev <yumindev@gmail.com>
2021-12-15 17:25:41 -0800

    init

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/types';
+import { TaskService } from 'src/app/services';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+}
